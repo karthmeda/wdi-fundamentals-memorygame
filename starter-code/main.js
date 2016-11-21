4820b9baefee2a62166c82c3f21a7e320b38eb29
karthmeda <karthmeda@gmail.com>
2016-11-21 13:15:58 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
+//Declared four card variables all as string with the values "queen" and "king" respectively 
+var cardOne="queen";
+var cardTwo="king";
+var cardThree="queen";
+var cardFour="king";
+
