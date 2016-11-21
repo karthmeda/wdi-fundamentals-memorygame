b82abae1b5216cada62fa36e970913bd14217f41
karthmeda <karthmeda@gmail.com>
2016-11-21 17:56:44 -0500

    created function and logic in javascript to generate 4 cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,14 +6,36 @@ var cardThree="queen";
 var cardFour="king";
 
 //If-else statements to check for equality of the four cards
-if(cardOne===cardThree||cardTwo===cardFour){
-	alert("Found a match");
+// if(cardOne===cardThree||cardTwo===cardFour){
+// 	alert("Found a match");
+// }
+// else if(cardOne===cardFour||cardTwo===cardThree||cardOne===cardTwo||cardThree===cardFour){
+// 	alert("Found a match");
+// }
+// else{
+// 	alert("Sorry, try again.")
+// }
+
+
+//variable that stores thr div element with 'game-board' id
+var makeBoard=document.getElementById('game-board');
+
+//createCards function to generate cards
+var createCards =function() 
+
+{
+
+for (var i=0;i<4;i++){
+	var newCard=document.createElement('div');
+	newCard.className="card";
+	makeBoard.appendChild(newCard);
+
 }
-else if(cardOne===cardFour||cardTwo===cardThree||cardOne===cardTwo||cardThree===cardFour){
-	alert("Found a match");
+
 }
-else{
-	alert("Sorry, try again.")
-}
+
+createCards();
+
+
 
 
